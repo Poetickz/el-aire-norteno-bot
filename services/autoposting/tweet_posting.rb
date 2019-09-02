ebbf31a86e2688d051af538ce204f85dfc54e369
Alan Rocha <alan.rocha@udem.edu>
2019-09-01 20:01:21 -0500

    Created aqi method

--- services/autoposting/tweet_posting.rb
+++ services/autoposting/tweet_posting.rb
@@ -1,3 +1,24 @@
 require_relative '../../secrets/ruby_secrets'
+require 'nokogiri'
+require 'open-uri'
 
-puts @restClient
+def get_aqi(city)
+  puts city
+  url = "https://aqicn.org/city/mexico/nuevo-leon/#{city}"
+  doc = Nokogiri::HTML(open(url))
+  doc.css("div#aqiwgtvalue")[0].text.to_i
+end
+
+
+time = 1800
+
+  
+  # if  not (doc.css(".tags .tag").text.include?("futanari") || doc.css(".tags .tag").text.include?("yaoi"))
+  #   @restClient.update("#{tweet}")
+  #   puts tweet
+
+  # else
+  #   time = 1
+  # end
+
+
