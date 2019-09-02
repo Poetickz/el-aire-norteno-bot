58dd0e91e3962084a268af3154f277263d1e485a
Alan Rocha <alan.rocha@udem.edu>
2019-09-01 22:30:19 -0500

    Fixed configuration ruby path

--- secrets/ruby_secrets.rb
+++ secrets/ruby_secrets.rb
@@ -1,13 +1,18 @@
 require 'yaml'
 require 'twitter'
 
-twitter_keys = YAML.load(File.read("../../secrets/twitter-keys.yml"))
+begin
+  twitter_keys = YAML.load(File.read("secrets/twitter-keys.yml"))
+rescue => exception
+    
+end
+
 
 config = {
-    consumer_key:        twitter_keys["twitter-keys"]["api-key"],
-    consumer_secret:     twitter_keys["twitter-keys"]["api-secret-key"],
-    access_token:        twitter_keys["twitter-keys"]["access-key"],
-    access_token_secret: twitter_keys["twitter-keys"]["access-secret-key"]
+    consumer_key:        ENV["API-KEY"] ||= twitter_keys["twitter-keys"]["api-key"],
+    consumer_secret:     ENV["API-SECRET-KEY"] ||= twitter_keys["twitter-keys"]["api-secret-key"],
+    access_token:        ENV["ACCESS-KEY"] ||= twitter_keys["twitter-keys"]["access-key"],
+    access_token_secret: ENV["ACCESS-SECRET-KEY"] ||= twitter_keys["twitter-keys"]["access-secret-key"]
 }
 
 @restClient = Twitter::REST::Client.new(config)
