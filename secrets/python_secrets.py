cc19f981bb43eb7ebb3241f0a86f63b290188880
Gladys <gladys.garza@udem.edu>
2019-09-03 00:35:03 -0500

    first try DONT ADD UP

--- secrets/python_secrets.py
+++ secrets/python_secrets.py
@@ -1,5 +1,20 @@
 import yaml
+import tweepy
+
 with open('twitter-keys.yml', 'r') as f:
     doc = yaml.load(f)
     
 print (doc["twitter-keys"]["api-key"])
+
+# Authenticate to Twitter
+#auth = tweepy.OAuthHandler("api-key", "api-secret-key:")
+#auth.set_access_token("access-key", "access-secret-key")
+
+# Create API object
+#api = tweepy.API(auth)
+
+#try:
+#    api.verify_credentials()
+#    print("Authentication OK")
+#except:
+#    print("Error during authentication")
--- services/replying/replying_tweet.py
+++ services/replying/replying_tweet.py
@@ -1,1 +1,25 @@
-print "Listo"
+#!/usr/bin/env python
+import tweepy
+#from our keys module (twitter-keys.py), import the keys dictionary
+from twitter-keys import twitter-keys
+
+CONSUMER_KEY = keys['api-key']
+CONSUMER_SECRET = keys['api-secret-key']
+ACCESS_TOKEN = keys['access-key']
+ACCESS_TOKEN_SECRET = keys['access-secret-key']
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+twts = api.search(q="Hello Wor!")     
+
+#list of specific strings we want to check for in Tweets
+t = ['Hello wor!',
+    'Hello Wor!]
+
+for s in twt:
+    for i in t:
+        if i == s.text:
+            sn = s.user.screen_name
+            m = "@%s Hello!" % (sn)
+            s = api.update_status(m, s.id)
