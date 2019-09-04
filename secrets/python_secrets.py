ebe5d8c09a2c26dd7233285835dbe47c4dc38d68
Gladys <gladys.garza@udem.edu>
2019-09-04 17:05:59 -0500

    python secrets complete & requirements update with tweepy libraries

--- secrets/python_secrets.py
+++ secrets/python_secrets.py
@@ -1,20 +1,13 @@
 import yaml
 import tweepy
 
-with open('twitter-keys.yml', 'r') as f:
-    doc = yaml.load(f)
-    
-print (doc["twitter-keys"]["api-key"])
+# load the yaml file
+document = open('twitter-keys.yml', 'r')
+# and finally parse the file
+parsed = yaml.load(document)
 
-# Authenticate to Twitter
-#auth = tweepy.OAuthHandler("api-key", "api-secret-key:")
-#auth.set_access_token("access-key", "access-secret-key")
-
-# Create API object
-#api = tweepy.API(auth)
-
-#try:
-#    api.verify_credentials()
-#    print("Authentication OK")
-#except:
-#    print("Error during authentication")
+#keys yei!!
+consumer_key = parsed["twitter-keys"]["api-key"]
+consumer_secret = parsed["twitter-keys"]["api-secret-key"]
+access_token = parsed["twitter-keys"]["access-key"]
+access_token_secret = parsed["twitter-keys"]["access-secret-key"]
