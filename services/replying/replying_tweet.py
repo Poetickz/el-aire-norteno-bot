106a15ad6a61822f02f65bd994401554b7174dd6
Gladys <gladys.garza@udem.edu>
2019-09-05 00:34:11 -0500

    python respond

--- services/replying/replying_tweet.py
+++ services/replying/replying_tweet.py
@@ -1,13 +1,48 @@
-import tweepy
+import tweepy, time
 import os, sys
 sys.path.insert(0, "./secrets/")
 from python_secrets import *
 
-
+#from last_seen_id import *
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("consumer_key", "consumer_secret")
-auth.set_access_token("access_token", "access_token_secret")
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+auth.secure = True
+api = tweepy.API(auth)
 
-# Create API object
-api = tweepy.API(auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True)
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...', flush=True)
+   
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    
+    mentions = api.mentions_timeline(
+                        last_seen_id,
+                        tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text, flush=True)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        if '#helloworld' in mention.full_text.lower():
+            print('found #helloworld!', flush=True)
+            print('responding back...', flush=True)
+            api.update_status('@' + mention.user.screen_name +
+                    '#HelloWorld back to you!', mention.id)
+
+while True:
+    print("yo pueddooo")
+    reply_to_tweets()
+    time.sleep(15)
