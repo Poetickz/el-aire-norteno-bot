c4c1c047ba4996fa24c6f94e85c2a01fcd7de2c4
Alan Rocha <alan.rocha@udem.edu>
2019-09-05 19:38:03 -0500

    Quickfix

--- config/clock_autoposting_service.rb
+++ config/clock_autoposting_service.rb
@@ -1,6 +1,6 @@
 while true
   puts "**************************************************************"
-  puts " Publish at: #{Time.now.getlocal('-08:00')} of #{Date.now} "
+  puts " Publish at: #{Time.now.getlocal('-08:00')} of #{Time.now.strftime("%d/%m/%Y")} "
   system("ruby services/autoposting/tweet_posting.rb")
   puts ".............................................................."
   sleep 28780
