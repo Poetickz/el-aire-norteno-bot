bc9ec1cfcb34264a9ef1295129be2f54feacdf5f
Alan Rocha <alan.rocha@udem.edu>
2019-09-11 20:45:49 -0500

    Added ENV variables

--- secrets/python_secrets.py
+++ secrets/python_secrets.py
@@ -1,13 +1,18 @@
 import yaml
+import os
 import tweepy
 
-# load the yaml file
-document = open('secrets/twitter-keys.yml', 'r')
-# and finally parse the file
-parsed = yaml.load(document)
 
-#keys yei!!
-consumer_key = parsed["twitter-keys"]["api-key"]
-consumer_secret = parsed["twitter-keys"]["api-secret-key"]
-access_token = parsed["twitter-keys"]["access-key"]
-access_token_secret = parsed["twitter-keys"]["access-secret-key"]
+if os.path.exists('secrets/twitter-keys.yml'):
+    document = open('secrets/twitter-keys.yml', 'r')
+    # and finally parse the file
+    parsed = yaml.load(document)    
+    consumer_key = parsed["twitter-keys"]["api-key"]
+    consumer_secret = parsed["twitter-keys"]["api-secret-key"]
+    access_token = parsed["twitter-keys"]["access-key"]
+    access_token_secret = parsed["twitter-keys"]["access-secret-key"]
+else:
+    consumer_key = os.environ['API-KEY']
+    consumer_secret = os.environ['API-SECRET-KEY']
+    access_token = os.environ['ACCESS-KEY']
+    access_token_secret = os.environ['ACCESS-SECRET-KEY']
