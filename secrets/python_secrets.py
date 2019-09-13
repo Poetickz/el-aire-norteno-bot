24b43357589d085a58bcf2ef37f41504ca1ff1d3
Alan Rocha <alan.rocha@udem.edu>
2019-09-13 00:49:08 -0500

    Quick fix json file

--- secrets/python_secrets.py
+++ secrets/python_secrets.py
@@ -15,18 +15,17 @@ if os.path.exists('secrets/el-aire-norteno-bot-firebase-adminsdk-mae3g-6348a4cdc
     })
 else:
     firebase_admin.initialize_app( credentials.Certificate({
-    "type": os.environ["FIREBASE_TYPE"],
-    "project_id": "el-aire-norteno-bot",
-    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
-    "private_key": os.environ["FIREBASE_PRIVATE_KEY"],
-    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
-    "client_id": os.environ["FIREBASE_CLIENT_ID"],
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"]}), {
-    'databaseURL': 'https://el-aire-norteno-bot.firebaseio.com'
-    })
+        "type": os.environ["FIREBASE_TYPE"],
+        "project_id": "el-aire-norteno-bot",
+        "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+        "private_key": os.environ["FIREBASE_PRIVATE_KEY"],
+        "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+        "client_id": os.environ["FIREBASE_CLIENT_ID"],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"]}), 
+    {'databaseURL': 'https://el-aire-norteno-bot.firebaseio.com'})
 
 
 fb_db = db.reference()
