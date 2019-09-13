06d180913eceb011322c456855b2f7aa3d4a4431
Alan Rocha <alan.rocha@udem.edu>
2019-09-13 00:52:01 -0500

    Quick fix json file

--- secrets/python_secrets.py
+++ secrets/python_secrets.py
@@ -23,8 +23,7 @@ else:
         "client_id": os.environ["FIREBASE_CLIENT_ID"],
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"]}), 
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs"}), 
     {'databaseURL': 'https://el-aire-norteno-bot.firebaseio.com'})
 
 
