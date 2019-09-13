7e87230ff90fe760cb6882789dfd3ef0633b757b
Alan Rocha <alan.rocha@udem.edu>
2019-09-13 00:44:51 -0500

    Quick fix json file

--- secrets/python_secrets.py
+++ secrets/python_secrets.py
@@ -24,8 +24,9 @@ else:
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"]}),
-    'databaseURL': 'https://el-aire-norteno-bot.firebaseio.com'})
+    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"]}), {
+    'databaseURL': 'https://el-aire-norteno-bot.firebaseio.com'
+    })
 
 
 fb_db = db.reference()
