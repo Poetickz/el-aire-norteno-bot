072267675f33824e2bc6e87d5b9d9d7b1b8cdd30
Alan Rocha <alan.rocha@udem.edu>
2019-09-13 00:38:57 -0500

    Quick fix json file

--- secrets/python_secrets.py
+++ secrets/python_secrets.py
@@ -10,14 +10,24 @@ from firebase_admin import db
 
 if os.path.exists('secrets/el-aire-norteno-bot-firebase-adminsdk-mae3g-6348a4cdc1.json'):
     cred = credentials.Certificate("secrets/el-aire-norteno-bot-firebase-adminsdk-mae3g-6348a4cdc1.json")
-else:
-    with open(os.environ['FIREBASE-JSON-FILE'], 'w') as json_file:
-        json.dump(os.environ['FIREBASE-JSON'], json_file)
-    cred = credentials.Certificate(os.environ['FIREBASE-JSON-FILE'])
-
-firebase_admin.initialize_app(cred, {
+    firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://el-aire-norteno-bot.firebaseio.com'
-})
+    })
+else:
+    firebase_admin.initialize_app({
+    credential: admin.credential.cert({
+    "type": os.environ["FIREBASE_TYPE"],
+    "project_id": "el-aire-norteno-bot",
+    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"],
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ["FIREBASE_CLIENT_ID"],
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.environ["FIREBASE_CLIENT_X509_CERT_URL"]}),
+    'databaseURL': 'https://el-aire-norteno-bot.firebaseio.com'})
+
 
 fb_db = db.reference()
 
